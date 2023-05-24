58e91d237439db2c1b7b64f31c327d1f6f234501
MahmutAtia~ <mahmoud.atia.ead@gmail.com>
2023-05-24 21:05:24 +0300

    extintions

--- timer/background.js
+++ timer/background.js
@@ -1,26 +1,13 @@
 chrome.alarms.create({ periodInMinutes: 1 / 60 });
 
-
-chrome.alarms.onAlarm.addListener((alarm)=>{
-
-
-    chrome.storage.local.get("timer").then((res) => {
-       
-        const timer = res.timer ?? 0;
-        chrome.storage.local.set({ timer: timer + 1 });
-        console.log(timer);
-        chrome.action.setBadgeText({ text: timer.toString() });
-        this.registration.showNotification("Vibration Sample", {
-          body: "Buzz! Buzz!",
-        });
-
-        
-
-       
-      })
-
-     
-
-}
-  );
-  
+chrome.alarms.onAlarm.addListener((alarm) => {
+  chrome.storage.local.get("timer").then((res) => {
+    const timer = res.timer ?? 0;
+    chrome.storage.local.set({ timer: timer + 1 });
+    console.log(timer);
+    chrome.action.setBadgeText({ text: timer.toString() });
+    this.registration.showNotification("Vibration Sample", {
+      body: "Buzz! ",
+    });
+  });
+});
